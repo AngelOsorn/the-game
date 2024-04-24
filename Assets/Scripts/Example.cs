94c84feddecae973f9e3f1ba6857ecf23a5223bd
deeg-p23 <23diegopereyra@gmail.com>
2024-04-23 18:53:11 -0700

    Added built-in Land Navigation script and setup Animal prefabs

---
+++ Assets/Scripts/Example.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    int framesCounted;
+    bool counting;
+
+    void Start()
+    {
+        framesCounted = 0;
+    }
+
+    void Update()
+    {
+        framesCounted++;
+        Debug.Log(framesCounted + " " + counting);
+    }
+}
